365f1f12f4c4d7bcd30c28fa63c028e3a57d5cad
Dejan Miličić <dejan.milicic@gmail.com>
2025-04-05 18:18:41 +0200

    comment

--- CodeGen/Class1.cs
+++ CodeGen/Class1.cs
@@ -1,3 +1,4 @@
 ﻿// FSHARPFIX
 // Orleans generates C# code for serialization so we need a C# project to compile the generated code
+// Point this attribute to the assembly where Grains are
 [assembly: GenerateCodeForDeclaringAssembly(typeof(Grains.HelloGrain))]
