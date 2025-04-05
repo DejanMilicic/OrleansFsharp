bc65ff307dc6f934ac806eeb58e963a229fd4c36
Dejan Miličić <dejan.milicic@gmail.com>
2025-04-05 17:22:42 +0200

    F# serialization fix for Orleans 9

--- CodeGen/Class1.cs
+++ CodeGen/Class1.cs
@@ -1,2 +1,3 @@
-﻿// Orleans generates C# code for serialization so need a C# project to compile the generated code
+﻿// FSHARPFIX
+// Orleans generates C# code for serialization so we need a C# project to compile the generated code
 [assembly: GenerateCodeForDeclaringAssembly(typeof(Grains.HelloGrain))]
--- Grains/HelloGrain.fs
+++ Grains/HelloGrain.fs
@@ -3,13 +3,31 @@
 open System.Threading.Tasks
 open Orleans
 
+// FSHARPFIX
+// following three lines are necessary to make the Orleans codegen work
+// "CodeGen" is the name of the C# codegen project
+open System.Runtime.CompilerServices
+[<assembly: InternalsVisibleTo("CodeGen")>]
+do()
+
+// FSHARPFIX
+// the following line is necessary to make the Orleans codegen work
+[<GenerateSerializer>]
+type GreeterMessage =
+    | Greeting of string
+    | Number of int
+
 type IHelloGrain =
     inherit IGrainWithStringKey
-    abstract member SayHello : greeting:string -> Task<string>
+    abstract member Receive : msg:GreeterMessage -> Task<string>
 
 type HelloGrain() = 
     inherit Grain()
     
     interface IHelloGrain with
-        member this.SayHello(greeting:string) =
-            Task.FromResult($"Hello, You said: {greeting}")
+        member this.Receive(msg: GreeterMessage) =
+            match msg with
+            | Greeting greeting ->
+                Task.FromResult($"Greeter received greeting: *{greeting}*")
+            | Number number ->
+                Task.FromResult($"Greeter received number: *{number}*")
